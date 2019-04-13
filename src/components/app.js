14c66b5385da6e48b75f34cfd63e29542df9c15a
Eugene <eshereverov@gmail.com>
2019-04-13 23:54:42 +0300

    clean project

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h2>Redux SandBox!!!</h2>
+    </div>
+  );
+}
+
+export default App;
