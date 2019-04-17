da71ea4d91f9c7d68a08c0d649d178a20c0d266a
Eugene <eshereverov@gmail.com>
2019-04-17 21:42:35 +0300

    add bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-import { createStore } from 'redux';
-import {inc, dec, rnd} from './actions';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const { dispatch } = store;
+/*
+----analog bindActionCreators from redux----
+const bindActionCreator = (creator, dispatch) => (...arg) => dispatch(creator(...arg))
+*/
 const update = () => {
   document
     .getElementById('counter')
     .innerHTML = store.getState()
 }
 
+// bindActionCreators return object with fields
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
 store.subscribe(update)
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => store.dispatch(inc()))
+  .addEventListener('click', inc)
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => store.dispatch(dec()))
+  .addEventListener('click', dec)
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random()*10)
-    store.dispatch(rnd(payload))
+    rnd(payload)
   })
